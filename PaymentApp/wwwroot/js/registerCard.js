6fc1bd44a705fb47d080e1541dec11be57e52853
studentboy87 <thomasjedwards87@gmail.com>
2018-11-23 12:14:01 +0000

    update js events working

--- PaymentApp/wwwroot/js/registerCard.js
+++ PaymentApp/wwwroot/js/registerCard.js
@@ -1,37 +1,28 @@
 ﻿$(document).ready(function () {
-    //$('#registerCard').data("validator").settings.submitHandler = function (form) {
-    //    $('#btnSubmit').attr("disabled", true).val("Working");
-    //    form.submit();
-    //};
-    //$('form').submit(function () {
-    //    if ($(this).valid()) {
-    //        $('btnSubmit').attr("disabled", true).val();
-    //    }
-    //});
-
-
-//var validation= function validateFrom() {  $('#registerCard').validate({
-//        rules: {
-//            CardNumberString: {},
-//            CardOwnersName: {},
-//            CardAddress.AddressLine1{},
-
-
-//        }
-//})}
-
-    //var $form = $("#registerCard");
-    //var $submitbutton = $("#btnSubmit");
 
     $("#registerCard").on("blur", "input", () => {
         if ($("#registerCard").valid()) {
             $("#btnSubmit").removeAttr("disabled");
-            console.log("here");
         } else {
-            $$("#btnSubmit").attr("disabled", "disabled");
+            $("#btnSubmit").attr("disabled", "disabled");
         }
     });
 
+    function lastFourDigits(cardNum, cardNumModal) {
+        $(cardNum).bind('keypress keyup blur', function () {
+            var fullCardNum = $(cardNum).val();
+            var trimmedCardNum = fullCardNum.slice(12, 16);
+            var ast = "****-****-****-";
+            $(cardNumModal).val(ast + trimmedCardNum);
+        });
+
+    }
+
+    $('#CardNumberString').change(lastFourDigits($('#CardNumberString'), $('#modalCardNum')));
+
+    $(".holderName").change(function () {
+        $('#modalCardOwnerName').val($(this).val());
+    });
 
 });
 
